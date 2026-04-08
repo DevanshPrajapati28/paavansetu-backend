d4405bcba52d03a3952ea44c9fd2e788ad478390
devansh_283 <devanshprajapati99@gmail.com>
2026-04-08 13:32:30 +0530

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -5,13 +5,15 @@ const nodemailer = require('nodemailer');
 // ─── SMTP TRANSPORTER ─────────────────────────────────────
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // 🔥 SSL
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 // Verify transporter
 transporter.verify((error) => {
   if (error) {
