11b31bbdf6ea89acb1a24d9ae369a01e5f2e95a4
devansh_283 <devanshprajapati99@gmail.com>
2026-04-08 13:34:33 +0530

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -4,16 +4,17 @@ const nodemailer = require('nodemailer');
 
 // ─── SMTP TRANSPORTER ─────────────────────────────────────
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  family: 4,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
-
 // Verify transporter
 transporter.verify((error) => {
   if (error) {
